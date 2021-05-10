f1b8469c95acd70936aa8e85367e0530d5b73a02
myyrakle <sssang97@naver.com>
2021-05-10 13:39:14 +0000

    0.0.5

---
+++ index.js
@@ -0,0 +1,8 @@
+function run(){
+    //const name = process.argv[1];
+    console.log('인자값: ', process.argv);
+}
+
+run();
+
+module.exports = run;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-const pluralize = require('pluralize');
-
-function main(){
-    const name = process.argv[1];
-    console.log('인자값: ', name);
-}
