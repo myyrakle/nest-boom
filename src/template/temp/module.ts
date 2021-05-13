e813b3b7fc8ab9d43f8d1ea89de14df7ea498021
myyrakle <sssang97@naver.com>
2021-05-13 09:14:13 +0900

    0.6.1 복수형 처리 개선

--- src/template/temp/module.ts
+++ src/template/temp/module.ts
@@ -5,7 +5,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { <%= classify(singular(name)) %> } from './<%= singular(name) %>.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([<%= classify(name) %>])],
+  imports: [SequelizeModule.forFeature([<%= classify(singular(name)) %>])],
   controllers: [<%= classify(name) %>Controller],
   providers: [<%= classify(name) %>Service], 
   exports: [<%= classify(name) %>Service], 
