5ef294a9bbf9a0486f518239643af8d0870d537b
sanggeon <sssang97@svcorps.dev>
2021-05-13 09:04:43 +0900

    0.5.2

--- src/template/temp/module.ts
+++ src/template/temp/module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { <%= classify(name) %>Service } from './<%= name %>.service';
 import { <%= classify(name) %>Controller } from './<%= name %>.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { <%= classify(name) %> } from './<%= name %>.model';
+import { <%= classify(singular(name)) %> } from './<%= singular(name) %>.model';
 
 @Module({
   imports: [SequelizeModule.forFeature([<%= classify(name) %>])],
