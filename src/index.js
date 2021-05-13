a8e44bc613082ea3d9c50c4f2b031caaa020e6e0
sanggeon <sssang97@svcorps.dev>
2021-05-13 09:00:44 +0900

    복수형 처리 수정

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 #! /usr/bin/env node
 
-const {classify, dasherize} = require('@angular-devkit/core/src/utils/strings');
-const {singular} = require('pluralize');
-const ejs = require('ejs');
-const fse = require('fs-extra');
-const path = require('path');
-const { writeFile, unlink, access, rename } = require('fs');
-const { promisify } = require('util');
+const {
+    classify,
+    dasherize,
+} = require("@angular-devkit/core/src/utils/strings");
+const { singular } = require("pluralize");
+const ejs = require("ejs");
+const fse = require("fs-extra");
+const path = require("path");
+const { writeFile, unlink, access, rename } = require("fs");
+const { promisify } = require("util");
+const { green, yellow, red } = require("chalk");
 
-const writeFileAsync  = promisify(writeFile);
-const unlinkAsync  = promisify(unlink);
-const accessAsync  = promisify(access);
-const renameAsync = promisify(rename)
+const writeFileAsync = promisify(writeFile);
+const unlinkAsync = promisify(unlink);
+const accessAsync = promisify(access);
+const renameAsync = promisify(rename);
+
+console.green = (...args) => console.log(green(args));
+console.yellow = (...args) => console.log(yellow(args));
+console.red = (...args) => console.log(red(args));
 
 const lowercased = (name) => {
     const classifiedName = classify(name);
-    return (
-        classifiedName.charAt(0).toLowerCase() + classifiedName.slice(1)
-    );
+    return classifiedName.charAt(0).toLowerCase() + classifiedName.slice(1);
 };
 
 const ejsContext = {
@@ -27,26 +33,26 @@ const ejsContext = {
     lowercased,
 };
 
-let basePath = '.';
+let basePath = ".";
 
-async function run(){
-    const [_1,_2, name] = process.argv;
+async function run() {
+    const [_1, _2, name] = process.argv;
 
-    if(!name){
-        console.log(`사용 형태`)
-        console.log(`----------`)
-        console.log(`nest-boom <리소스 이름>`)
-        console.log(`----------`)
-        console.log(`(리소스 이름은 snake case)`)
+    if (!name) {
+        console.log(`사용 형태`);
+        console.log(`----------`);
+        console.log(`nest-boom <리소스 이름>`);
+        console.log(`----------`);
+        console.log(`(리소스 이름은 snake case)`);
         return;
     }
 
     ejsContext.name = name;
 
-    const file_path = path.join(__dirname, 'template');
+    const file_path = path.join(__dirname, "template");
 
-    if(accessAsync('./src')) {
-        basePath = './src';
+    if (accessAsync("./src")) {
+        basePath = "./src";
     }
 
     await fse.copy(file_path, basePath);
@@ -55,52 +61,70 @@ async function run(){
         model(name),
         module(name),
         service(name),
-        createDto(name), 
+        createDto(name),
         updateDto(name),
     ]);
 
-    await renameAsync(`${basePath}/temp`, `${basePath}/${singular(name)}`);
+    await renameAsync(`${basePath}/temp`, `${basePath}/${name}`);
 
-    console.log('생성 완료');
+    console.log("생성 완료");
 }
 
-async function controller(name){
-    const content = await ejs.renderFile(`${basePath}/temp/controller.ts`, ejsContext);
-    const newFileName = `${basePath}/temp/${singular(name)}.controller.ts`;
+async function controller(name) {
+    const content = await ejs.renderFile(
+        `${basePath}/temp/controller.ts`,
+        ejsContext
+    );
+    const newFileName = `${basePath}/temp/${name}.controller.ts`;
     await writeFileAsync(newFileName, content);
     await unlinkAsync(`${basePath}/temp/controller.ts`);
 }
 
-async function model(name){
-    const content = await ejs.renderFile(`${basePath}/temp/model.ts`, ejsContext);
+async function model(name) {
+    const content = await ejs.renderFile(
+        `${basePath}/temp/model.ts`,
+        ejsContext
+    );
     const newFileName = `${basePath}/temp/${singular(name)}.model.ts`;
     await writeFileAsync(newFileName, content);
     await unlinkAsync(`${basePath}/temp/model.ts`);
 }
 
-async function module(name){
-    const content = await ejs.renderFile(`${basePath}/temp/module.ts`, ejsContext);
-    const newFileName = `${basePath}/temp/${singular(name)}.module.ts`;
+async function module(name) {
+    const content = await ejs.renderFile(
+        `${basePath}/temp/module.ts`,
+        ejsContext
+    );
+    const newFileName = `${basePath}/temp/${name}.module.ts`;
     await writeFileAsync(newFileName, content);
     await unlinkAsync(`${basePath}/temp/module.ts`);
 }
 
-async function service(name){
-    const content = await ejs.renderFile(`${basePath}/temp/service.ts`, ejsContext);
-    const newFileName = `${basePath}/temp/${singular(name)}.service.ts`;
+async function service(name) {
+    const content = await ejs.renderFile(
+        `${basePath}/temp/service.ts`,
+        ejsContext
+    );
+    const newFileName = `${basePath}/temp/${name}.service.ts`;
     await writeFileAsync(newFileName, content);
     await unlinkAsync(`${basePath}/temp/service.ts`);
 }
 
-async function createDto(name){
-    const content = await ejs.renderFile(`${basePath}/temp/dto/create-__name@singular__.dto.ts`, ejsContext);
+async function createDto(name) {
+    const content = await ejs.renderFile(
+        `${basePath}/temp/dto/create-__name@singular__.dto.ts`,
+        ejsContext
+    );
     const newFileName = `${basePath}/temp/dto/create-${singular(name)}.dto.ts`;
     await writeFileAsync(newFileName, content);
     await unlinkAsync(`${basePath}/temp/dto/create-__name@singular__.dto.ts`);
 }
 
-async function updateDto(name){
-    const content = await ejs.renderFile(`${basePath}/temp/dto/update-__name@singular__.dto.ts`, ejsContext);
+async function updateDto(name) {
+    const content = await ejs.renderFile(
+        `${basePath}/temp/dto/update-__name@singular__.dto.ts`,
+        ejsContext
+    );
     const newFileName = `${basePath}/temp/dto/update-${singular(name)}.dto.ts`;
     await writeFileAsync(newFileName, content);
     await unlinkAsync(`${basePath}/temp/dto/update-__name@singular__.dto.ts`);
