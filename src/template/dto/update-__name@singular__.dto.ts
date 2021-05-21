c56373f57dcc9aa3fd82d13eb544bf4772c5287d
myyrakle <sssang97@naver.com>
2021-05-21 11:31:36 +0900

    updatedto에 partialtype 적용

--- src/template/dto/update-__name@singular__.dto.ts
+++ src/template/dto/update-__name@singular__.dto.ts
@@ -1,10 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
+import { Create<%= singular(classify(name)) %>Dto } from './create-<%= singular(name) %>.dto';
 
-export class Update<%= singular(classify(name)) %>Dto {
-    @ApiProperty({
-        example: 'boom',
-        description: '값',
-        required: true,
-    })
-    value: string;
+export class Update<%= singular(classify(name)) %>Dto extends PartialType(Create<%= singular(classify(name)) %>Dto) {
 }
