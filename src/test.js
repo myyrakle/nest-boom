ecfb90a4ffa71f75c09a983f5af1d45f73381aeb
myyrakle <sssang97@naver.com>
2021-05-28 00:00:21 +0000

    테스트소스 추가

--- src/test.js
+++ src/test.js
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
 
+const { readFileSync } = require('fs');
+
 const typescript = require('typescript');
+const { transpileModule } = typescript;
 
-const program = typescript.createProgram(['./app.module.ts'], {maxNodeModuleJsDepth: 1});
-const test = program.getSourceFiles()[0].getLastToken();
+const code = String(readFileSync('./app.module.ts'));
 
-console.log(test);
+console.log(transpileModule(code));
